74db344b44df780b520e2ad9d1e069b1710533a1
mightyfrog <mightyfrog.gc@gmail.com>
2017-04-15 00:14:57 +0900

    Initial commit

---
+++ app/src/main/java/org/mightyfrog/android/movinggradientbackgroundsample/MainActivity.java
@@ -0,0 +1,26 @@
+package org.mightyfrog.android.movinggradientbackgroundsample;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+/**
+ * http://thetechnocafe.com/make-a-moving-gradient-background-in-android/
+ *
+ * @author Shigehiro Soejima
+ */
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    FrameLayout layout = (FrameLayout) findViewById(R.id.content_frame);
+    AnimationDrawable animationDrawable = (AnimationDrawable) layout.getBackground();
+    animationDrawable.setEnterFadeDuration(2500);
+    animationDrawable.setExitFadeDuration(5000);
+    animationDrawable.start();
+  }
+}
